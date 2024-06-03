f96e566999f40f02b324ee7ed6112eef7241a0ce
D050513 <sebastian.van.syckel@sap.com>
2024-06-03 11:45:09 +0200

    revised handler

--- bupa/srv/bupa.js
+++ bupa/srv/bupa.js
@@ -2,10 +2,17 @@ const cds = require('@sap/cds'), { UPDATE } = cds.ql
 
 module.exports = srv => {
   srv.after('READ', 'A_BusinessPartnerAddress', async function (addresses) {
+    const { timestamp: readAt, user: { id: readBy } } = cds.context
     for (const address of addresses) {
       const { AddressID, BusinessPartner } = address
-      const { timestamp: readAt, user: { id: readBy } } = cds.context
       await UPDATE('db.A_BusinessPartnerAddress', { AddressID, BusinessPartner }).set({ readAt, readBy })
     }
   })
+
+  // srv.after('READ', 'A_BusinessPartnerAddress', async function (addresses) {
+  //   const { timestamp: readAt, user: { id: readBy } } = cds.context
+  //   await UPDATE('db.A_BusinessPartnerAddress')
+  //     .set({ readAt, readBy })
+  //     .where(`concat(AddressID,':',BusinessPartner) in`, addresses.map(a => `${a.AddressID}:${a.BusinessPartner}`))
+  // })
 }
